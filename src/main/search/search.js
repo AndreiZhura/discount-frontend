48704a4edde9b7087ff93d887de3f8210c206e61
AndreiZhura <zhura2296@yandex.ru>
2023-07-23 21:38:15 +0300

    сделал стрелочку у поиска

--- src/main/search/search.js
+++ src/main/search/search.js
@@ -2,17 +2,21 @@ import React from "react";
 import '../search/search.css';
 import lupa from "../../img/lupa.svg";
 import { Link } from 'react-router-dom'; 
+import enter from "../../img/enter.svg"
 
 function Search() {
     return (
         <div className="search-form">
             <form className="search-form__forma">
                 <img className="lupa" alt="поиск" src={lupa} />
+                <div className="seach-form__search">
                 <input className="seach-form__input"
                     placeholder="Поиск"
                     type="search"
                     name="search"
                 />
+                <button className="search-form__enter"></button>
+                </div>
             </form>
         </div>
     );
