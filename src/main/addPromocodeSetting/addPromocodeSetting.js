9a859b51042f91f19cc3f7387ca9adb0d8e44d42
AndreiZhura <zhura2296@yandex.ru>
2023-08-18 21:28:22 +0300

    добавление промокода из настроек

--- src/main/addPromocodeSetting/addPromocodeSetting.js
+++ src/main/addPromocodeSetting/addPromocodeSetting.js
@@ -2,23 +2,28 @@ import React, { useState } from "react";
 import '../../settingsDiscount/settingsDiscount'
 
 function AddPromoSetting(props) {
+     
 
+    const [description, setDescription] = useState('')
     const [promocode, setPromocode] = useState('');
     const [date, setDate] = useState('');
  
     
     function handleSubmitPromo(e){
         e.preventDefault();
-        props.handlePromo(promocode, date,props.infoDiscount._id)
+        props.handlePromo(description,promocode, date,props.infoDiscount._id)
         props.setCounter(0);
     }
+
+   function handleDescription(e){
+    setDescription(e.target.value)
+   }
 
     function handlePromocode(e) {
         setPromocode(e.target.value);
     }
 
     function handleData(e) {
-
         setDate(e.target.value);
     }
 
@@ -27,6 +32,14 @@ function AddPromoSetting(props) {
         <>
             <div>
                 <div className="input__promocode">
+                <textarea
+                    className="input__text input__text_description-promocode"
+                    id="add-text"
+                    name="add-text"
+                    placeholder="описание промокода"
+                    onChange={handleDescription}
+                >
+                </textarea>
                     <input type="text"
                         placeholder="Промокод"
                         className="input__add-promocode"
