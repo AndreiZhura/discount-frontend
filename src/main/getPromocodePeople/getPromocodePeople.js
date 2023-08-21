9f41ad05a6877e1278935eb2d75dc1661311e886
AndreiZhura <zhura2296@yandex.ru>
2023-08-21 10:26:12 +0300

    исправил положение даты

--- src/main/getPromocodePeople/getPromocodePeople.js
+++ src/main/getPromocodePeople/getPromocodePeople.js
@@ -5,10 +5,10 @@ import '../../lookDiscount/lookDiscount.css'
 function GetPromocodePeople(props) {
 
     const date = new Date(props.value.date)
-    console.log(props.value.description.length)
+
     return (
         <>
-            <div className="input__promocode">
+            <div className={ props.value.promocode ? "input__promocode" : "input__promocode input__promocode_no-promocode"}>
                 {
                     props.value.description ? <p className={props.value.description.length > 200
                         ?
@@ -16,7 +16,10 @@ function GetPromocodePeople(props) {
                         : 
                         "input__text look__text-padding input__text_description-promocode input__text_small"}>{props.value.description}</p> : <></>
                 }
-                <p className="input__add-promocode look__text-padding look__promocode-margin">{props.value.promocode}</p>
+                {
+                    props.value.promocode ?   
+                    <p className="input__add-promocode look__text-padding look__promocode-margin">{props.value.promocode}</p>:<></>
+                }
                 <p className="input__add-date look__add-date look__text-padding">до {date.toLocaleDateString()}</p>
             </div>
         </>
