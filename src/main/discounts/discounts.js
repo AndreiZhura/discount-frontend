f48b8d1a826bcafdd2abec751fce00b205d09e5e
AndreiZhura <zhura2296@yandex.ru>
2023-08-24 19:11:34 +0300

    сделал иконки еще меньше на маленьком разрешении

--- src/main/discounts/discounts.js
+++ src/main/discounts/discounts.js
@@ -7,7 +7,7 @@ import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
-//import 'swiper/css/navigation';
+import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 //import 'swiper/css/scrollbar';
 
