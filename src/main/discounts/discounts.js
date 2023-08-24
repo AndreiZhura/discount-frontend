5b3275980d636409d754f74884fcea85565690e3
AndreiZhura <zhura2296@yandex.ru>
2023-08-24 16:38:10 +0300

    сделал что бы каждая карточка отражалась у своей категории

--- src/main/discounts/discounts.js
+++ src/main/discounts/discounts.js
@@ -16,26 +16,33 @@ function Discounts(props) {
         <div className="discounts">
            <Swiper
                 spaceBetween={50}
-                slidesPerView={2}
+                slidesPerView={3}
                 onSlideChange={() => console.log('slide change')}
                 onSwiper={(swiper) => console.log(swiper)}
             >
               
-                {
+                { props.discount.length === 0 ? <></> :
                      props.discount.map((value)=>{
                            return(
+                            props.category._id === value.category ?
                             <SwiperSlide>
                                     <div className="discount">
                                     <div className="discount__main">
                                         < img className="discount__image" src={`${BASE_URL}` + `${value.image}`} />
                                     </div>
                                 </div>
-                            </SwiperSlide>
+                            </SwiperSlide> : <></>
                            )
                      })
                 }
     
             </Swiper>
+            <AddDiscount
+              handleAddDiscount = {props.handleAddDiscount}
+              category = {props.category}
+              infoCategoryID = {props.infoCategoryID}
+              loggedIn = {props.loggedIn}
+            />
         </div>
     );
 }
